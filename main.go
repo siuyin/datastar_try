21f83c7da679bdcbb9da9a373b947e6b50631538
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-05-18 13:24:01 +0000

    feat: add server time

--- main.go
+++ main.go
@@ -5,16 +5,23 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"time"
+
+	datastar "github.com/starfederation/datastar/sdk/go"
 )
 
 func main() {
 	http.Handle("/", http.FileServerFS(public.Content))
+	http.HandleFunc("/time", timeHandler)
 	http.HandleFunc("/hello", helloHandler)
 
 	log.Println("Starting HTTP server...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	sse := datastar.NewSSE(w, r)
+	sse.MergeFragments(`<div id="mytime">`+time.Now().Format("15:04:05.0000")+`</div>`)
+}
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello World\n")
 }
