208aec31112e6214d7a3e02ac2020b0da1231614
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-05-18 18:51:10 +0800

    chore: formatting

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"log"
-	"io"
-	"net/http"
 	"example/public"
+	"io"
+	"log"
+	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.FileServerFS(public.Content))
-	http.HandleFunc("/hello",helloHandler)
+	http.HandleFunc("/hello", helloHandler)
 
 	log.Println("Starting HTTP server...")
-	log.Fatal(http.ListenAndServe(":8080",nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w,"Hello World\n")
+	io.WriteString(w, "Hello World\n")
 }
